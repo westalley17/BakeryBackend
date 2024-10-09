13d8c7f3d1ea495805726de7d943fbf020f2c423
Wes Talley <wesatalley@gmail.com>
2024-10-09 12:21:48 -0500

    Fixing emp timesheets

--- app.js
+++ app.js
@@ -618,7 +618,12 @@ app.get('/api/employee/employeeHours', async (req, res) => {
             res.status(401).json({ error: "Unauthorized access!" });
         }
         const response = await getEmpBiHours(user.UserID);
-        res.status(200).json(response);
+        if(!response) {
+            res.status(404).json({ error: "No clocked hours!" });
+        }
+        else {
+            res.status(200).json(response);
+        }
     } catch (error) {
         res.status(500).json({ error: 'Internal server error' });
     }
